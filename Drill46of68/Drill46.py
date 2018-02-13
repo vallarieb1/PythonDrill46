90d5568a2f1b75472d3bfb435b1bb7f4c3d7fb3e
vallarieb1 <v.bhuthongkum@gmail.com>
2018-02-13 11:08:49 -0800

    Python46of68
    3 Drills for Instructor's review. Please let me know if I should revise anything.

---
+++ Drill46of68/Drill46.py
@@ -0,0 +1,30 @@
+for i in range(4):
+    print(i)
+
+def list_multiply ():
+    numlist = [1000,100,10]
+    numrange = [i for i in range (3,0,-1)]
+    num_Total = (numlist[0]*numrange[0])+(numlist[1]*numrange[1])+(numlist[2]*numrange[2])
+    print ("\nHere is the answer for Python 46 of 68, Drill #2: ",num_Total)
+
+list_multiply()
+
+def list_multiply2 ():
+    numlist2 = [1000,100,10]
+    numrange2 = [n for n in range (8,0,-2)]
+    num_Total2 = (numlist2[0]*numrange2[0])+(numlist2[1]*numrange2[1])+(numlist2[2]*numrange2[2])+(numrange2[3])
+    print ("\nHere is the answer for Python 46 of 68, Drill #3: ",num_Total2)
+
+list_multiply2()
+
+
+
+
+    
+
+
+
+
+
+
+    
